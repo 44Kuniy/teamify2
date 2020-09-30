d442e6ddb6ca2ae41a7f212ea415348d1513bb88
44Kuniy <43056908+44Kuniy@users.noreply.github.com>
2020-09-30 19:57:17 +0900

    test commit

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -1,24 +1,15 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
 
 import Page from '../components/Page'
 import Container from '../components/Container'
 import IndexLayout from '../layouts'
+import MyFirstGrid from '../components/MyFirstGrid'
 
 const PageTwo = () => (
   <IndexLayout>
     <Page>
       <Container>
-        <h1>Hi from the second page</h1>
-        <p>Welcome to page 2</p>
-        <ul>
-          <li>
-            <Link to="/a-markdown-page/">Show me some Markdown!</Link>
-          </li>
-          <li>
-            <Link to="/">Take me back home.</Link>
-          </li>
-        </ul>
+        <MyFirstGrid />
       </Container>
     </Page>
   </IndexLayout>
